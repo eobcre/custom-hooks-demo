cc3723d07d60968f125ecaf1fa40506b1c6c2d77
eobcre <eobcre@gmail.com>
2023-04-30 14:15:19 -0400

    useState settings, button layout, increment and decrement button logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCounter = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
+  const decrementCounter = () => {
+    setCount((prevState) => prevState - 1);
+  };
+
   return (
     <div className='counter-container'>
       <h1>Counter Demo</h1>
+      <div className='counter'>
+        <h2>{count}</h2>
+        <div className='button-container'>
+          <button onClick={incrementCounter}>Increment</button>
+          <button onClick={decrementCounter}>Decrement</button>
+          <button>Clear</button>
+        </div>
+      </div>
     </div>
   );
 };
