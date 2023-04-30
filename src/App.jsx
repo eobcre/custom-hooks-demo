d16df0cd0018e9e375f9ad55939e470a208e0a4c
eobcre <eobcre@gmail.com>
2023-04-30 16:16:44 -0400

    render CounterOne and CounterTwo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
-import { useState } from 'react';
+// Components
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  const incrementCounter = () => {
-    setCount((prevState) => prevState + 1);
-  };
-
-  const decrementCounter = () => {
-    setCount((prevState) => prevState - 1);
-  };
-
-  const clearCounter = () => {
-    setCount(0);
-  };
-
   return (
     <div className='counter-container'>
       <h1>Counter Demo</h1>
-      <div className='counter'>
-        <h2>{count}</h2>
-        <div className='button-container'>
-          <button onClick={incrementCounter}>Increment</button>
-          <button onClick={decrementCounter}>Decrement</button>
-          <button onClick={clearCounter}>Clear</button>
-        </div>
-      </div>
+      <CounterOne />
+      <CounterTwo />
     </div>
   );
 };
